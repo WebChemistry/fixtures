675a9b0d69cdb992aaacf267d5e7491c4886326d
MartkCz <martk494@gmail.com>
2025-04-07 17:40:05 +0200

    feat: add SqlOnlyFixture

---
+++ src/SqlOnlyFixture.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types = 1);
+
+namespace WebChemistry\Fixtures;
+
+use RuntimeException;
+
+/**
+ * @template T of object
+ * @implements Fixture<T>
+ */
+abstract class SqlOnlyFixture implements Fixture
+{
+
+	public function dependencies(): array
+	{
+		return [];
+	}
+
+	final public function run(): iterable
+	{
+		return [];
+	}
+
+	final public function make(array $values = []): object
+	{
+		throw new RuntimeException('SQL only fixture cannot be used to create entities');
+	}
+
+	final public function makeMany(int $times, array $values = []): array
+	{
+		throw new RuntimeException('SQL only fixture cannot be used to create entities');
+	}
+
+}
